060dc17e309254b53506347722ebc7a76ebfa765
tarasandnes <john.kendall.reid@gmail.com>
2015-12-22 07:00:53 -0800

    Very first Commit
    Just created a Console App with a Beep. The very fucking beginning

---
+++ TheStorySky/Program.cs
@@ -0,0 +1,24 @@
+﻿// BK stuff that's greyed out here means stuff from package not being used - use reshaper alt enter then can remove
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// Classes (related) in namespaces - Namespaces in assemblies (DLLs etc..) - DLLs in/part of APP
+namespace TheStorySky
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press any key to start this fucking app");
+            Console.ReadKey();
+            Console.WriteLine("Fuck you world");
+            Console.Beep(220, 4000);
+
+            Console.ReadLine();
+        }
+    }
+}
